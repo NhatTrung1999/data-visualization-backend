30b70f1dc87318e4dbd4cec296c856f849e894c2
Trung Nguyen <trungnguyen15051999@gmail.com>
2025-08-02 16:15:13 +0700

    fix-bug-20250802

--- src/data-visualization/data-visualization.service.ts
+++ src/data-visualization/data-visualization.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateDataVisualizationDto } from './dto/create-data-visualization.dto';
-import { UpdateDataVisualizationDto } from './dto/update-data-visualization.dto';
 
 @Injectable()
 export class DataVisualizationService {}
